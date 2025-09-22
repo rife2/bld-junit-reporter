e39625d41c4218ff3483d6d1b9e7113fbb53d21a
Erik C. Thauvin <erik@thauvin.net>
2025-09-21 22:31:16 -0700

    Bump AssertJ to version 3.27.5

--- src/bld/java/rife/bld/extension/JUnitReporterBuild.java
+++ src/bld/java/rife/bld/extension/JUnitReporterBuild.java
@@ -52,7 +52,7 @@ public class JUnitReporterBuild extends Project {
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone",
                         version(1, 13, 4)))
                 .include(dependency("org.assertj", "assertj-core",
-                        version(3, 27, 4)))
+                        version(3, 27, 5)))
                 .include(dependency("org.mockito", "mockito-core",
                         version(5, 19, 0)));
         javadocOperation()
